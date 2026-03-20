34579daa03432aacd93fb9829406159b2ae6fffe
legacyunlimited <71147160+legacyunlimited@users.noreply.github.com>
2026-03-20 16:38:18 -0700

    Update strategies.py

--- strategies.py
+++ strategies.py
@@ -1,7 +1,32 @@
-import random
+import pandas as pd
 
-def orb_signal():
-    return random.choice(["LONG", "SHORT", None])
+def orb_signal(data):
+    # data = DataFrame with OHLC
+    first_15 = data.iloc[:3]  # 3 x 5-min candles
 
-def vwap_signal():
-    return random.choice(["LONG", "SHORT", None])
+    high = first_15['high'].max()
+    low = first_15['low'].min()
+
+    current_price = data.iloc[-1]['close']
+
+    if current_price > high:
+        return "LONG"
+    elif current_price < low:
+        return "SHORT"
+    else:
+        return None
+
+
+def vwap_signal(data):
+    # basic VWAP approximation
+    data['vwap'] = (data['close'] * data['volume']).cumsum() / data['volume'].cumsum()
+
+    current_price = data.iloc[-1]['close']
+    vwap = data.iloc[-1]['vwap']
+
+    if current_price < vwap * 0.98:
+        return "LONG"
+    elif current_price > vwap * 1.02:
+        return "SHORT"
+    else:
+        return None
